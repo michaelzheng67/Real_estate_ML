39fc046b342c9e5bbcad2a6948b4eba9ed89489f
Michael Zheng <michaelzheng@Michaels-MacBook-Pro.local>
2021-08-08 19:55:15 -0400

    added feature to simulation

--- simulation.py
+++ simulation.py
@@ -12,7 +12,24 @@ class Simulation:
         self.inflation = self.__random_inflation()
         self.cash_appreciation = self.__random_cash_appreciation() - self.__random_inflation() - self.__black_swan()
 
+    # Return new value methods
+    def new_interest_rate(self):
+        self.interest_rate = self.__random_interest_rate()
+        return self.interest_rate
+
+    def new_inflation(self):
+        self.inflation = self.__random_inflation()
+        return self.inflation
+
+    def new_cash_appreciation(self):
+        self.cash_appreciation = self.__random_cash_appreciation() - self.__random_inflation() - self.__black_swan()
+        return self.cash_appreciation
+
+    def new_house_array(self):
+        return abs(np.random.normal(700000, 1000000,10)) # returns array of 10 houses for player to purchase
+
     # Private methods to generate random variables for environment
+
     @staticmethod
     def __random_interest_rate():
         return np.random.normal(2, 2)
