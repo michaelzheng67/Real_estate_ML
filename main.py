d8e41aa944da33ace8b968c0b5c5291ef52d5d21
michael-lee <m.lee2002@yahoo.com>
2021-08-12 14:48:09 -0400

    - Implemented method to update property financial data with the passage of time - Altered some variables (appreciation, interest rates) from random to fixed for ease of testing
    Requests:
    - Random variables are being created at init of property class. This means they are static for the remainder of the simulation. Let's change this to be more dynamic. Also, do you think appreciation attribute of each property class should be a Beta of the overall market as opposed to independent rates?
    
    Signed-off-by: michael-lee <m.lee2002@yahoo.com>

--- main.py
+++ main.py
@@ -4,6 +4,18 @@
 
 
 # Modular imports
-from simulation import Simulation, Property
+from simulation import Simulation, Property, mortgage_financials, update_properties
 
-print(Simulation.new_house_array())
+Sim = Simulation()
+owned_properties = Sim.available_property # Temporary placeholder, before decision making is implemented
+
+for t in range (241):
+    print("Month:", t+1)
+    Sim = Simulation()
+    update_properties(owned_properties)
+
+
+    #print("Month:", t+1)
+    #for i in range(3):
+        #print("Purchase Price: {0:0.2f}, Rental Yield: {1:0.2f}%, Expenses Rate: {2:0.2f}%, Appreciation Rate: {3:0.2f}% ".format(Sim.available_property[i].purchase_price, Sim.available_property[i].rent_yield, Sim.available_property[i].expenses_rate, Sim.available_property[i].appreciation_rate))
+
